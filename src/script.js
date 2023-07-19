af13468123409196aa1337514964bebbc2737038
Sage Choi <coolcuds@gmail.com>
2023-07-19 14:19:13 -0700

    Add cursor and controls to rotate and move the object with the mouse

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 /**
  * Base
@@ -12,6 +13,19 @@ const sizes = {
     height: 600
 }
 
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+
+    console.log(cursor.x, cursor.y)
+});
+
 // Scene
 const scene = new THREE.Scene()
 
@@ -23,12 +37,22 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000) // (FOV, aspect ratio, near, and far)
+
+// Results in render area width larger than render area height bc of canvas width and height. Cube looks like a cube now
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls -- can accomplish same thing as update camera in tick
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true;
+// controls.target.y = 1;
+// controls.update();
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -44,7 +68,16 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+
+    // Update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position);
+
+    // Update controls
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
